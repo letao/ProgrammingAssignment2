4b2bbab58beb2757f1ab0971bb795206d8d5c78c
letao <davisozols@gmail.com>
2014-09-18 11:38:12 +0200

    Finished Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() is a function for setting up and catching the inverse of a matrix; 
+## function sets the value of "m" to NULL and then uses "m" as a placeholder 
+## for the catched inverse calculation via solve() in the function cachesolve()
 
-## Write a short comment describing this function
+## set() function allows to change the elements in the matrix, e.g. x$set(matrix(...)) 
+## in case a new matrix is created "m" is reset to NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+## get() function shows the elements in the matrix
+## getinverse() function shows the cached inverse, if no inverse is cached value is NULL
 
+## setinverse() function alows to set the value of "m" from the result in cachesolve(), 
+## this is the value retrived if no changes have been made to the matrix
+
+makeCacheMatrix  <- function(x = matrix()) {    
+      m  <- NULL                                                  
+      set  <- function(y) {                     
+            x <<- y 
+            m <<- NULL
+      }     
+      get  <- function() {x}
+      setinverse  <- function(inverse) {m  <- inverse} 
+      getinverse <- function() {m}
+      list(set = set,                           
+           get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cachesolve() calculates the inverse of a matrix by taking the value of "m" 
+## from makeCacheMatrix(); it uses an if statement that evaluates "m"; if "m" = NULL 
+## an inverse for the given matrix is calculated and saved as the new value of "m"; 
+## if m =/= NULL the current value of m is printed with the comment "getting cached data"
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## the caching is achieved via the manipulation of the variable "m", when a matrix 
+## is created or edited "m" = NULL, that prompts the use of solve() in the cachesolve() 
+## function that saves its output in "m", as long as m =/= NULL the inverse is retrived 
+## by printing "m"
+
+cachesolve  <- function(x, ...) {               
+      m  <- x$getinverse()                      
+      if(!is.null(m)) {                        
+            message("getting cached data")      
+            return(m)
+      }
+      data  <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)                           
+      m                                         
 }
